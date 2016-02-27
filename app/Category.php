e936235843e170f2b874cd5d8e61cbfc794f99e3
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2016-02-26 23:33:13 -0300

    Categorias, e Produtos criados

---
+++ app/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CodeCommerce;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+    $fillable = ['name', 'description'];
+}
---
+++ app/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CodeCommerce;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    $fillable = ['name', 'description', 'price'];
+    
+}
